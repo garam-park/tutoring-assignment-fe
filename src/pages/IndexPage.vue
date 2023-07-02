256a74dca46d00c33051ad0e0b6e8d4258ef9986
garam <garam-park@naver.com>
2023-07-02 11:41:45 +0900

    #5 토픽 카드 만들기 완료

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,49 +1,67 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <q-page>
-      <div class="justify-center q-gutter-lg">
-        <q-tab-panels v-model="grade" animated keep-alive>
-          <q-tab-panel name="입문">
-            <div>입문</div>
-          </q-tab-panel>
-          <q-tab-panel name="초급">
-            <div>초급</div>
-          </q-tab-panel>
-          <q-tab-panel name="중급">
-            <div>중급</div>
-          </q-tab-panel>
-          <q-tab-panel name="중고급">
-            <div>중고급</div>
-          </q-tab-panel>
-          <q-tab-panel name="고급">
-            <div>고급</div>
-          </q-tab-panel>
-        </q-tab-panels>
-      </div>
-      <q-header class="bg-white">
-        <q-toolbar class="text-primary">
-          <q-tabs
-            v-model="grade"
-            dense
-            align="justify"
-            narrow-indicator
-            class="full-width"
+  <q-page>
+    <q-tab-panels v-model="grade" animated keep-alive>
+      <q-tab-panel name="입문">
+        <div class="column wrap justify-center content-around row q-px-md">
+          <q-card
+            v-for="t in topics"
+            :key="t.idx"
+            flat
+            bordered
+            class="q-mb-md col full-width"
+            style="max-width: 400px; /* 태블릿 크기에 맞게 설정 */"
           >
-            <q-tab
-              v-for="op in options"
-              :name="op.value"
-              :label="op.label"
-              :key="op.value"
-            />
-          </q-tabs>
-        </q-toolbar>
-      </q-header>
-    </q-page>
+            <q-img :src="t.imgPath" :ratio="4 / 3">
+              <div class="absolute-bottom">
+                <div class="text-subtitle2 text-right">{{ t.grade }}</div>
+                <div class="text-h6">{{ t.title }}</div>
+              </div>
+            </q-img>
+
+            <q-card-actions align="right">
+              <q-btn flat round color="primary" icon="favorite_outline" />
+            </q-card-actions>
+          </q-card>
+        </div>
+      </q-tab-panel>
+      <q-tab-panel name="초급">
+        <div>초급</div>
+      </q-tab-panel>
+      <q-tab-panel name="중급">
+        <div>중급</div>
+      </q-tab-panel>
+      <q-tab-panel name="중고급">
+        <div>중고급</div>
+      </q-tab-panel>
+      <q-tab-panel name="고급">
+        <div>고급</div>
+      </q-tab-panel>
+    </q-tab-panels>
+    <q-header class="bg-white">
+      <q-toolbar class="text-primary">
+        <q-tabs
+          v-model="grade"
+          dense
+          align="justify"
+          narrow-indicator
+          class="full-width"
+        >
+          <q-tab
+            v-for="op in options"
+            :name="op.value"
+            :label="op.label"
+            :key="op.value"
+          />
+        </q-tabs>
+      </q-toolbar>
+    </q-header>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { readTopics } from 'src/stores/topic/topics.store';
+import Topic from 'src/entities/Topic';
 
 const grade = ref<string>('입문');
 
@@ -54,4 +72,15 @@ const options = [
   { label: '중고급', value: '중고급' },
   { label: '고급', value: '고급' },
 ];
+
+const topics = ref<Topic[]>([]);
+readTopics({ page: 1, per_page: 10, grade: '입문' }).then((ts) => {
+  topics.value = ts;
+});
 </script>
+
+<style>
+.q-tab-panel {
+  padding: 0px;
+}
+</style>
